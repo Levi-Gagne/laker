187c5b75651371ac6101a4012661e63cbcf8dbf7
Levi Gagne <67981022+Levi-Gagne@users.noreply.github.com>
2025-07-29 19:58:54 -0500

    Create snapshot.py

---
+++ src/layker/steps/snapshot.py
@@ -0,0 +1,80 @@
+# src/layker/steps/snapshot.py
+
+{
+    "table": {
+        "fully_qualified_name": "",
+        "catalog": "",
+        "schema": "",
+        "table": "",
+        "owner": "",
+        "comment": "",
+        "table_properties": {},
+        "table_tags": {
+            "example_tag": ""
+        },
+        "table_check_constraints": {
+            "constraint_1": {
+                "name": "",
+                "expression": ""
+            }
+        },
+        "row_filters": [
+            {
+                "filter_name": "",
+                "target_columns": ""
+            }
+        ],
+        "partitioned_by": [""],
+        "primary_key": [""],
+        "columns": {
+            1: {
+                "column_name": "",
+                "datatype": "",
+                "comment": "",
+                "nullable": None,
+                "masking_rule": None,
+                "column_tags": {
+                    "example_tag": ""
+                },
+                "column_check_constraints": {
+                    "constraint_1": {
+                        "name": "",
+                        "expression": ""
+                    }
+                }
+            },
+            2: {
+                "column_name": "",
+                "datatype": "",
+                "comment": "",
+                "nullable": None,
+                "masking_rule": None,
+                "column_tags": {
+                    "example_tag": ""
+                },
+                "column_check_constraints": {
+                    "constraint_1": {
+                        "name": "",
+                        "expression": ""
+                    }
+                }
+            },
+            3: {
+                "column_name": "",
+                "datatype": "",
+                "comment": "",
+                "nullable": None,
+                "masking_rule": None,
+                "column_tags": {
+                    "example_tag": ""
+                },
+                "column_check_constraints": {
+                    "constraint_1": {
+                        "name": "",
+                        "expression": ""
+                    }
+                }
+            }
+        }
+    }
+}
