443c4a3622765897149858b207746653c982b3f7
Levi Gagne <67981022+Levi-Gagne@users.noreply.github.com>
2025-07-31 19:45:56 -0500

    Update metadata.py

--- src/layker/metadata.py
+++ src/layker/metadata.py
@@ -4,6 +4,87 @@ from typing import List, Dict, Any, Optional
 import re
 from layker.utils.table import parse_fully_qualified_table_name
 
+"""
+{
+    "table": {
+        "fully_qualified_name": "",
+        "catalog": "",
+        "schema": "",
+        "table": "",
+        "owner": "",
+        "comment": "",
+        "table_properties": {},
+        "table_tags": {
+            "example_tag": ""
+        },
+        "table_check_constraints": {
+            "constraint_1": {
+                "name": "",
+                "expression": ""
+            }
+        },
+        "row_filters": [
+            {
+                "filter_name": "",
+                "target_columns": ""
+            }
+        ],
+        "partitioned_by": [""],
+        "primary_key": [""],
+        "columns": {
+            1: {
+                "column_name": "",
+                "datatype": "",
+                "comment": "",
+                "nullable": None,
+                "masking_rule": None,
+                "column_tags": {
+                    "example_tag": ""
+                },
+                "column_check_constraints": {
+                    "constraint_1": {
+                        "name": "",
+                        "expression": ""
+                    }
+                }
+            },
+            2: {
+                "column_name": "",
+                "datatype": "",
+                "comment": "",
+                "nullable": None,
+                "masking_rule": None,
+                "column_tags": {
+                    "example_tag": ""
+                },
+                "column_check_constraints": {
+                    "constraint_1": {
+                        "name": "",
+                        "expression": ""
+                    }
+                }
+            },
+            3: {
+                "column_name": "",
+                "datatype": "",
+                "comment": "",
+                "nullable": None,
+                "masking_rule": None,
+                "column_tags": {
+                    "example_tag": ""
+                },
+                "column_check_constraints": {
+                    "constraint_1": {
+                        "name": "",
+                        "expression": ""
+                    }
+                }
+            }
+        }
+    }
+}
+"""
+
 SNAPSHOT_QUERIES = {
     "table_tags": {
         "table": "system.information_schema.table_tags",
